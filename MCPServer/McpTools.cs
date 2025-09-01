f398032106943849bd7ba085c694ba6bf80e78e9
Justin Mätschke <justin.maetschke@gmail.com>
2025-09-01 20:09:34 +0200

    ,.-m

--- MCPServer/McpTools.cs
+++ MCPServer/McpTools.cs
@@ -19,7 +19,7 @@ namespace MCPServer
         public McpTools(string dbPath = "testdata.db")
         {
             _dbPath = dbPath;
-            InitializeDatabaseAsync();
+            InitializeDatabaseAsync().GetAwaiter().GetResult();
         }
 
         public async Task InitializeDatabaseAsync()
